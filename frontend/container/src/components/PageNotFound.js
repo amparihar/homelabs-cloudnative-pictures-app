cad1ff7ffc6544058f7c32798f44d900e741a8aa
Amit Parihar <Amit.Parihar@lplfinancial.com>
2021-04-13 23:07:25 +0530

    Added kustomization overlays

--- frontend/container/src/components/PageNotFound.js
+++ frontend/container/src/components/PageNotFound.js
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
+  const {
+    location: { href },
+  } = window;
   return (
-    <section className="section" style={{maxWidth:"50%"}}>
+    <section className="section" style={{ maxWidth: "50%" }}>
       <div className="container">
         <article className="message is-link">
           <div className="message-header">
-            <p>404</p>
+            <p>404 - Webpage not found</p>
           </div>
           <div className="message-body">
-            Oops! That page couldn't be found.
-            <br/>
+            The webpage at {href} might be temporarily down or it may have moved
+            permanently to a new web address.
+            <br />
             Please <Link to="/home">click here</Link> to go to the Home Page.
           </div>
         </article>
