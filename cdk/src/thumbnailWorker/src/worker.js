c1f7a9e919a49dfa1170cdf693e310509af7c339
EC2 Default User <ec2-user@ip-172-31-29-63.ap-south-1.compute.internal>
2021-12-13 07:33:04 +0000

    Updated queue processor worker task

--- cdk/src/thumbnailWorker/src/worker.js
+++ cdk/src/thumbnailWorker/src/worker.js
@@ -1,1 +1,21 @@
 const AWS = require("aws-sdk");
+
+
+var sqs = AWS.sqs(),
+    queueUrl = process.env.THUMBNAIL_QUEUE;
+
+var receiveMessageParams = {
+    QueueUrl: queueUrl,
+    MaxNumberOfMessages: 10,
+    
+};
+
+sqs.receiveMessage(receiveMessageParams, (err, data) => {
+    if (err) {
+        console.log("Receive Message Error", err);
+    }
+    else {
+        console.log("Received Messages", data);
+    }
+})
+
